99edcf5ac912375f4e8bba93a78656390f183717
lotuspay <srmilho@proton.me>
2025-09-07 15:20:07 -0300

    git

--- bancoparaimportar.sql
+++ bancoparaimportar.sql
@@ -17,10 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `megagr80_projeto`
---
-
+-
 -- --------------------------------------------------------
 
 --
